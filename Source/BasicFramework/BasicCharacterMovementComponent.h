4b74fdf3f7d79b4c1c23c0dbfa6e23fbccae2c17
Giulio  M <1172898+giuliom@users.noreply.github.com>
2017-12-09 17:42:15 +0100

    added BasicCharacterMovementComponent

---
+++ Source/BasicFramework/BasicCharacterMovementComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "BasicCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICFRAMEWORK_API UBasicCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
